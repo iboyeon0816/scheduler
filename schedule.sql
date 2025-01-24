9f32a1b528a1a5263fbc708cdd7296447def6caf
iboyeon0816 <iboyeon0816@gmail.com>
2025-01-24 12:01:11 +0900

    chore: Author 테이블을 생성하는 DDL 추가

--- schedule.sql
+++ schedule.sql
@@ -3,9 +3,17 @@ CREATE DATABASE scheduler;
 
 USE scheduler;
 
+CREATE TABLE Author (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL
+);
+
 CREATE TABLE schedule (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(30)	NOT NULL,
+    author_id BIGINT NOT NULL REFERENCES Author (id),
     password VARCHAR(30) NOT NULL,
     task TEXT NOT NULL,
     created_at timestamp NOT NULL,
