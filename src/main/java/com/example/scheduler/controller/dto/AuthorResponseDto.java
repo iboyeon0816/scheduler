4db75982115ccf08306dd81e8718307912ad6cfb
iboyeon0816 <iboyeon0816@gmail.com>
2025-01-27 15:41:57 +0900

    feat: AuthorResponseDto 필드명 변경

--- src/main/java/com/example/scheduler/controller/dto/AuthorResponseDto.java
+++ src/main/java/com/example/scheduler/controller/dto/AuthorResponseDto.java
@@ -7,14 +7,14 @@ import java.time.LocalDateTime;
 
 @Getter
 public class AuthorResponseDto {
-    private final Long id;
+    private final Long authorId;
     private final String name;
     private final String email;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
 
     public AuthorResponseDto(Author author) {
-        this.id = author.getId();
+        this.authorId = author.getId();
         this.name = author.getName();
         this.email = author.getEmail();
         this.createdAt = author.getCreatedAt();
